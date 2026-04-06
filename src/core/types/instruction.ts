c6145d251af6706a6120c735ea4dc11011405f6d
sekito <sekito1107gg@gmail.com>
2026-04-07 04:49:16 +0900

    Sync YARV instruction types with Ruby 4.0.2

--- src/core/types/instruction.ts
+++ src/core/types/instruction.ts
@@ -1,6 +1,13 @@
 import { RubyValue } from './ruby_value';
 
+export interface CallData {
+  mid: string;
+  flag: number;
+  orig_argc: number;
+}
+
 export type Instruction =
+  | { name: 'putobject_INT2FIX_1_' }
   | { name: 'putobject'; operand: RubyValue }
-  | { name: 'opt_plus' }
+  | { name: 'opt_plus'; operand: CallData }
   | { name: 'leave' };
